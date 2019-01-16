2cf89f63287ce90c52fede43cd7834e729ff9fd6
Alfonso Esteve <alfonsem@gmail.com>
2019-01-16 15:42:18 +0100

    creamos los componentes necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { ListComponent } from './list/list.component';
+import { ShowListsComponent } from './show-lists/show-lists.component';
+import { DataManagerComponent } from './data-manager/data-manager.component';
+import { TaskComponent } from './task/task.component';
+import { AddNewListComponent } from './add-new-list/add-new-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    ListComponent,
+    ShowListsComponent,
+    DataManagerComponent,
+    TaskComponent,
+    AddNewListComponent
   ],
   imports: [
     BrowserModule,
